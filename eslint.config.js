481e07336c3fda1ad1d11300fb7f871871c95ae6
King-3 <75344974+coderking3@users.noreply.github.com>
2025-11-19 09:59:11 +0000

    temp commit

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,5 @@ import { king3 } from '@king-3/eslint-config'
 export default king3(
   {
     typescript: true
-  },
-  {
-    rules: {
-      'antfu/no-import-dist': 'off'
-    }
   }
 )
--- prettier.config.ts
+++ prettier.config.ts
@@ -1,4 +1,4 @@
-import { king3 } from './dist/index.js'
+import { king3 } from './src/index'
 
 export default king3({
   printWidth: 120
