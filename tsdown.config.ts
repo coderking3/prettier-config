0b8b76f5b548e267fd8c599e9678b2aa989369ab
King-3 <75344974+coderking3@users.noreply.github.com>
2025-11-20 05:39:51 +0000

    🚀 feat: add Oxc plugin and drop CJS support, switching project to ESM-only

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,23 +1,7 @@
-import type { UserConfig } from 'tsdown'
-
 import { defineConfig } from 'tsdown'
 
-const createConfigs = (baseConfig: UserConfig, configs: UserConfig[]) => {
-  const config = configs.map((config) => ({
-    ...baseConfig,
-    ...config
-  })) as UserConfig
-  return config
-}
-
-const baseConfig: UserConfig = {
+export default defineConfig({
   entry: ['./src/index.ts'],
-  clean: true
-}
-
-export default defineConfig(
-  createConfigs(baseConfig, [
-    { format: 'esm', dts: true },
-    { format: 'cjs', dts: false }
-  ])
-)
+  shims: true,
+  format: ['esm']
+})
